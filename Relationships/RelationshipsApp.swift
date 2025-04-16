ea8806ac82c7c8eeb012110724eeed0699fae2e6
ItsWilder <david@itswilder.com>
2025-04-16 15:22:49 -0400

    Initial Commit

---
+++ Relationships/RelationshipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelationshipsApp.swift
+//  Relationships
+//
+//  Created by David Wilder on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct RelationshipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
